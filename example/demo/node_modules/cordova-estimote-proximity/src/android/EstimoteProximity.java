31f8a9cd9da472584b06b9bfb5a5e0cc6b205fc5
Zack Hable <zach.hable@pngaming.com>
2018-06-14 12:47:58 -0400

    added support for cloud credentials

--- example/demo/node_modules/cordova-estimote-proximity/src/android/EstimoteProximity.java
+++ example/demo/node_modules/cordova-estimote-proximity/src/android/EstimoteProximity.java
@@ -2,6 +2,7 @@ package com.zackhable.estimote;
 
 // Java Libs
 import java.util.List;
+import java.util.ArrayList;
 // Cordova Libs
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
@@ -21,13 +22,20 @@ import kotlin.jvm.functions.Function1;
 // Custom Libs (3rd Party)
 import com.estimote.mustard.rx_goodness.rx_requirements_wizard.Requirement;
 import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory;
+import com.estimote.proximity_sdk.proximity.EstimoteCloudCredentials;
 import com.estimote.proximity_sdk.proximity.ProximityObserver;
 import com.estimote.proximity_sdk.proximity.ProximityObserverBuilder;
 
 public class EstimoteProximity extends CordovaPlugin {
 	
-	public static final String PLUGIN_NAME = "EstimoteProximity";
+	private static final String PLUGIN_NAME = "EstimoteProximity";
+	
+	private static Context appContext = null;
+	private static Activity appActivity = null;
+	
 	private static boolean hasBluetoothAccess = false;
+	private static EstimoteCloudCredentials cloudCredentials = null;
+	private static List<ProximityObserver> proximityObservers = null;
 	
 	/**
      * Sets the context of the Command. This can then be used to do things like
@@ -39,6 +47,9 @@ public class EstimoteProximity extends CordovaPlugin {
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
 		Log.d(PLUGIN_NAME, "Initializing Estimote Proximity class...");
+		appActivity = cordova.getActivity();
+		appContext = cordova.getContext();
+		proximityObservers = new ArrayList();
 	}
 	
 	/**
@@ -54,7 +65,16 @@ public class EstimoteProximity extends CordovaPlugin {
 			getBluetoothPermissions();
 		}
 		else if ("hasSystemPermissions".equals(action)) {
-			callbackContext.sendPluginResult(new PluginResult(Status.OK, hasBluetoothAccess));
+			callbackContext.sendPluginResult(new PluginResult(Status.OK, hasBluetoothAccess()));
+		}
+		else if ("setCloudCredentials".equals(action)) {
+			callbackContext.sendPluginResult(new PluginResult(Status.OK, setCloudCredentials(args)));
+		}
+		else if ("hasCloudCredentials".equals(action)) {
+			callbackContext.sendPluginResult(new PluginResult(Status.OK, hasCloudCredentials()));
+		}
+		else if ("buildProximityObserver".equals(action)) {
+			callbackContext.sendPluginResult(new PluginResult(Status.OK, buildProximityObserver(args)));
 		}
 		else { // endpoint doesn't exist
 			return false;
@@ -65,13 +85,86 @@ public class EstimoteProximity extends CordovaPlugin {
 	
 	/*** PLUGIN FUNCTIONS ***/
 	
+	private int buildProximityObserver(JSONArray args) {
+		if (!hasBluetoothAccess()) {
+			getBluetoothPermissions();
+		}
+		if (hasCloudCredentials()) {
+			try {
+				proximityObservers.add(ProximityObserverBuilder(appContext, cloudCredentials)
+                .withAnalyticsReportingDisabled()
+                .withTelemetryReportingDisabled()
+                .build());
+				return proximityObservers.size()-1;
+			}
+			catch (JSONException e) {
+				Log.e(PLUGIN_NAME, e.getStackTrace());
+			}
+		}
+		return -1;
+	}
+	
+	private boolean startProximityObserver(JSONArray args) {
+		try {
+			int pid = args.getInt(0);
+			if (isValidPid(pid)) {
+				proximityObservers.get(i).start();
+			}
+			else {
+				return false;
+			}
+		}
+		catch (JSONException e) {
+			Log.e(PLUGIN_NAME, e.getStackTrace());
+			return false;
+		}
+		return true;
+	}
+	
+	
+	private boolean stopProximityObserverHandler(JSONArray args) {
+		
+	}
+	
+	private boolean isValidPid(int pid) {
+		if (pid >= proximityObservers.size()) {
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean hasCloudCredentials() {
+		if (cloudCredentials == null) {
+			Log.w(PLUGIN_NAME, "No Cloud Credentials have been initialized yet!");
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean setCloudCredentials(JSONArray args) {
+		try {
+			String appId = args.getString(0);
+			String appToken = args.getString(1);
+			cloudCredentials = new EstimoteCloudCredentials(appId, appToken);
+		}
+		catch (JSONException e) {
+			Log.e(PLUGIN_NAME, e.getStackTrace());
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean hasBluetoothAccess() {
+		return hasBluetoothAccess;
+	}
+	
 	/**
 	 * Ensures that the app has access to the necessary system resources
 	 */
 	private void getBluetoothPermissions() {
 		Log.d(PLUGIN_NAME, "Requesting access to system resources...");
 		RequirementsWizardFactory.createEstimoteRequirementsWizard().fulfillRequirements(
-			cordova.getActivity(), 
+			appActivity, 
 				new Function0<Unit>() {
 				@Override
 				public Unit invoke() {
