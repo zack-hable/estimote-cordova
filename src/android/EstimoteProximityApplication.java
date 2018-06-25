b14f91c95d5c45760937d21c4efb6758e43b0054
Zack Hable <zach.hable@pngaming.com>
2018-06-25 16:33:52 -0400

    added fixes from example app

--- src/android/EstimoteProximityApplication.java
+++ src/android/EstimoteProximityApplication.java
@@ -22,11 +22,11 @@ public class EstimoteProximityApplication extends Application implements Bootstr
         // type.  Do a web search for "setBeaconLayout" to get the proper expression.
         beaconManager.getBeaconParsers().add(new BeaconParser()
 		.setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
-		beaconManager.setBackgroundBetweenScanPeriod(45000);
-		beaconManager.setRegionStatePeristenceEnabled(false); // only use when debugging or possible for user to get multiple launches in location
+		//beaconManager.setBackgroundBetweenScanPeriod(45000);
+		beaconManager.setRegionStatePeristenceEnabled(false); // only use when debugging, possible for user to get multiple launches in location
 		Log.i(TAG, "background between scans: "+beaconManager.getBackgroundBetweenScanPeriod());
         // wake up the app when any beacon is seen (you can specify specific id filers in the parameters below)
-        Region region = new Region("com.zackhable.estimote.beaconRegion", null, null, null);
+        Region region = new Region("com.zackhable.estimote.beaconRegion", Identifier.parse("E1F54E02-1E23-44E0-9C3D-512EB56ADEC9"), null, null);
         regionBootstrap = new RegionBootstrap(this, region);
     }
 
